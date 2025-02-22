a1966a3a2e7e26a2d354d3a536d68d84060238ff
Cjey <cjvillanueva770@gmail.com>
2025-02-23 01:10:04 +0800

    haha

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   
