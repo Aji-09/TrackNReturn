fa20eb4ad234d39ab51fcc0424697acc16a73282
Cjey <cjvillanueva770@gmail.com>
2025-06-29 21:23:11 +0800

    Save before Tailwind upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
+
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
   modules: ["@nuxt/ui", "nuxt-vuefire", "@nuxtjs/cloudinary"],
   css: ["~/assets/css/main.css"],
+
   ui: {
     theme: {
       colors: [
@@ -33,12 +29,13 @@ export default defineNuxtConfig({
       apiKey: "AIzaSyBXh-IZ7LIWWmQVjcH20jbv2O7ex-s978w",
       authDomain: "tnts-tracknreturns.firebaseapp.com",
       projectId: "tnts-tracknreturns",
-      storageBucket: "tnts-tracknreturns.firebasestorage.app",
+      storageBucket: "tnts-tracknreturns.appspot.com",
       messagingSenderId: "869293817790",
       appId: "1:869293817790:web:df0079c2e2a9db26e143ef",
       measurementId: "G-T85MEMP8VW",
     },
   },
+
   typescript: {
     tsConfig: {
       compilerOptions: {
@@ -46,4 +43,6 @@ export default defineNuxtConfig({
       },
     },
   },
+  
+  compatibilityDate: "2025-06-29",
 });
